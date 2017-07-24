f5b7669a1eba62379b769a9ff9584945dff1775b
shafeeqonline <shafeeq.rahman01@gmail.com>
2017-07-24 08:20:50 -0400

    Editable popup for blocking posts on new request

--- content.js
+++ content.js
@@ -1,22 +1,33 @@
-// Listen for messages
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    // If the received message has the expected format...
-    if (msg.text === 'hide_blocked_content') {
-        var blockingArray = ["junior", "Pizza", "Saath"];
+(function () {
+    function hideBlockedContent(blockingArray) {
         var lowerCaseBlockingArray = blockingArray.join('|').toLowerCase().split('|');
         var allUserContent = document.querySelectorAll('.fbUserContent');
         [].forEach.call(allUserContent, function (contentBlock) {
             var textBlock = contentBlock.querySelector('.userContent'),
                 allTagsInside = textBlock.querySelectorAll('*');
-            allTagsInside.forEach(function(textTag){
-                debugger;
+            allTagsInside.forEach(function (textTag) {
                 if (new RegExp(lowerCaseBlockingArray.join("|")).test(textTag.innerText.toLowerCase())) {
                     // At least one match
                     contentBlock.className += ' content-blocked';
                 }
             })
         });
-
-        sendResponse("Done removed all userContent");
     }
-});
+
+    // Listen for messages
+    chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+        // If the received message has the expected format...
+        if (msg.text === 'hide_blocked_content') {
+            var blockingArray = ["junior", "Pizza", "Saath"];
+            // hideBlockedContent(blockingArray);
+
+            sendResponse("Done removed all userContent");
+        }
+    });
+    //Listen to storage change
+    chrome.storage.onChanged.addListener(function (changes, namespace) {
+        var blockingArray = changes.allWordsArray;
+        hideBlockedContent(blockingArray.newValue);
+    });
+
+})();
